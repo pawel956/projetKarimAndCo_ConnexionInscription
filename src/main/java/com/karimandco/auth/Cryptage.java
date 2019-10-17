14f01a3169aae477467fffd74c09dab0b67656f0
Pawel RADYNA <radyna.pawel@gmail.com>
2019-10-17 09:12:54 +0200

    ajout cryptage en sha256

---
+++ src/main/java/com/karimandco/auth/Cryptage.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.karimandco.auth;
+
+import java.security.MessageDigest;
+
+/**
+ *
+ * @author Damien
+ */
+public class Cryptage {
+
+    /**
+     * Cette méthode permet de crypter un string en sha256.
+     *
+     * @param base String
+     * @return String crypté en sha256
+     */
+    public static String sha256(String base) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            StringBuffer hexString = new StringBuffer();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+
+        } catch (Exception ex) {
+
+            throw new RuntimeException(ex);
+        }
+    }
+}
